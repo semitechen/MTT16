02a83a102a78ccd3fed1ca2bf65e6708de0d41c5
Kacper Jelonek <kapijelonek@gmail.com>
2025-08-24 17:11:45 +0200

    feat: add base firmware for motherboard

---
+++ motherboard/software/pico_sdk_import.cmake
@@ -0,0 +1,1 @@
+include(${CMAKE_CURRENT_LIST_DIR}/pico-sdk/external/pico_sdk_import.cmake)
---
+++ motherboard/software/src/main.c
@@ -0,0 +1,11 @@
+#include "pico/stdlib.h"
+#include "display.h"
+#include "midi.h"
+#include "sd_card.h"
+#include "keyboard.h"
+
+int main() {
+    while (1) {
+
+    }
+}
